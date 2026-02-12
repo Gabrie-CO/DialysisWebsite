3e188bd2058c86c79f8204e4e50bed0cd53d0a67
JosephGabrie <josephgabriemolina@gmail.com>
2026-02-12 16:20:36 -0500

    docker, I barely know her

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import favicon from "$lib/assets/favicon.svg";
 	import { setupConvex } from "convex-svelte";
-	import { PUBLIC_CONVEX_URL } from "$env/static/public";
+	import { env } from "$env/dynamic/public";
 	import "../app.css";
 	let { children } = $props();
 
-	setupConvex(PUBLIC_CONVEX_URL);
+	setupConvex(env.PUBLIC_CONVEX_URL);
 </script>
 
 <svelte:head>
